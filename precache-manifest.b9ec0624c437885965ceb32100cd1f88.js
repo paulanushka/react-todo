8bee1ff7bc2608e93523872e039669233c2ceec7
Anushka Paul <anushkapaul1103@gmail.com>
2020-09-23 00:33:57 +0530

    Updates

---
+++ precache-manifest.b9ec0624c437885965ceb32100cd1f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3700c8451e95ca0b95184c88970bd49",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "560805914f4c853c1d17",
+    "url": "/react-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/react-todo/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560805914f4c853c1d17",
+    "url": "/react-todo/static/js/main.09efe7a1.chunk.js"
+  },
+  {
+    "revision": "b11c347c96c58c778121",
+    "url": "/react-todo/static/js/runtime-main.570cc3bc.js"
+  }
+]);
